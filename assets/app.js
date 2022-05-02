2128ebf85ab58e32447c068b69d1c22e2dfa86d6
00014973 <fozilzayniddinov7777@gmail.com>
2022-05-02 23:50:07 +0500

    all commited

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: false,
+    nav: false,
+    dots: false,
+    slidesPerView: 4,
+    allowTouchMove: true,
+    spaceBetween: 16,
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    }
+});
